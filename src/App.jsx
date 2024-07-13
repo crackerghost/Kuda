f026f4f6f17c2ec8ff8583c078dc02e3566a6575
RAJ <singhrajtilak64@gmail.com>
2024-07-13 07:06:24 +0530

    night

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,41 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import './App.css'
 import { BrowserRouter ,Routes,Route } from 'react-router-dom'
 import Home from './Components/Home/Home'
 
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [loader, setLoader] = useState(false)
+
+  useEffect(()=>{
+    const checkLoad = ()=>{
+      setTimeout(() => {
+        setLoader(true);
+      },1000);
+      
+    }
+    
+    window.addEventListener("load", checkLoad)
+
+    return () => {
+      window.removeEventListener('load', checkLoad);
+    };
+   
+  },[])
+
+  if(!loader){
+  return(
+    <div className='loader'>
+      <img src="/assets/Landingpage/loading.gif" alt="" />
+    </div>
+  )
+  }
 
   return (
     
     <BrowserRouter>
     <Routes>
-      <Route path='/' element={<Home/>}/>
+    <Route path='/' element={<Home/>}/>
     </Routes>
     </BrowserRouter>  
     
